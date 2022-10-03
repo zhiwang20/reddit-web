68c4bddc651528eafa9cd85cf5865cfabb690885
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-10-02 20:05:44 -0400

    fix signup and check amplify console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,11 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 import type { AppProps } from "next/app";
 
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+
+Amplify.configure({ ...awsconfig, ssr: true });
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // Remove the server-side injected CSS.
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -89,7 +89,12 @@ export default function Signup() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-      <Grid container direction="column" alignItems="center" justify="center">
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
         <Grid item>
           <TextField
             variant="outlined"
