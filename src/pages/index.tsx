191c8eb878a2c6069bd26dc40d4110757032aad1
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-10-02 22:27:18 -0400

    add all components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,44 @@
-import type { NextPage } from "next";
-import { useUser } from "../context/AuthContext";
+import React, { useEffect, useState } from "react";
+import { Container } from "@material-ui/core";
+import { API } from "aws-amplify";
+import { listPosts } from "../graphql/queries";
+import { ListPostsQuery, Post } from "../API";
+import PostPreview from "../components/PostPreview";
 
-const Home: NextPage = () => {
-  const { user } = useUser();
-  console.log("the user is", user);
-  return <div>hello world</div>;
-};
+export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
 
-export default Home;
+  useEffect(() => {
+    const fetchPostsFromApi = async (): Promise<Post[]> => {
+      const allPosts = (await API.graphql({ query: listPosts })) as {
+        data: ListPostsQuery;
+        errors: any[];
+      };
+
+      if (allPosts.data) {
+        setPosts(allPosts.data.listPosts.items as Post[]);
+        return allPosts.data.listPosts.items as Post[];
+      } else {
+        throw new Error("Could not get posts");
+      }
+    };
+
+    fetchPostsFromApi();
+  }, []);
+
+  return (
+    <Container maxWidth="md">
+      {posts.map((post) => (
+        <PostPreview key={post.id} post={post} />
+      ))}
+    </Container>
+  );
+}
+
+// Get all the posts on the server-side
+// Since all users can read posts in our schema login
+// we can use the API Key Authorization method
+
+// So we'll call some code to access our GraphQL API on the serverside
+// Pass it to our function as props
+// Render the posts on the home page to look like reddit posts
