642d4007d41d13914bdc13dc1c059d68e6189a8d
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-10-03 22:11:34 -0400

    amplify add storage and amplify push commands

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "redditCloneImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
